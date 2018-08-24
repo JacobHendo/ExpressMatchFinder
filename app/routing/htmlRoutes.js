ebb72c2aea772c7f8a7b1c6e73527f3d62f13361
Jacob Henderson <Hendersonjacob1492@gmail.com>
2018-08-24 09:45:24 -0400

    rearrange routes api

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,15 @@
 var path = require("path");
+
 module.exports =  (app)=>{
+    console.log("routes hookup");
+    app.get("/survey", (req, res)=>{
+        console.log("route hit");
+        
+        res.sendFile(path.join(__dirname,  '../public/survey.html'));
+    });
+    
     app.get("*", (req, res)=>{        
         res.sendFile(path.join(__dirname,  '../public/home.html'));
-    })
-    app.get("/survey", (req, res)=>{
-        res.sendFile(path.join(__dirname,  '../public/survey.html'));
-    })
+        console.log("home route hit");
+    });
 }
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 //Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+
 
 // Express Setup
 var app = express();
 var PORT = process.env.PORT || 3000;
-// Routes
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
 
 // Data Parsing
 app.use(express.static('app/public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//use static files
+app.use(express.static('public'));
+
+// Routes
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
 
 // Start up the Server
 app.listen(PORT, ()=> {
